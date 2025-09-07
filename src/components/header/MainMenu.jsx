136a7bbc6fcb895db7f73174356a3f8e91d86e93
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2025-09-07 17:04:11 +0530

    Update MainMenu.jsx

--- src/components/header/MainMenu.jsx
+++ src/components/header/MainMenu.jsx
@@ -9,42 +9,31 @@ const MainMenu = ({ toggleSubMenu, navbarPlacement }) => {
                     <Link href="/" className="active">Home</Link>
                 </li>
                 <li className="dropdown">
-                    <Link href="#" className="dropdown-toggle" data-toggle="dropdown" onClick={toggleSubMenu}>Pages</Link>
+                    <Link href="#" className="dropdown-toggle" data-toggle="dropdown" onClick={toggleSubMenu}>About</Link>
                     <ul className="dropdown-menu">
                         <li><Link href="/about-us">About Us</Link></li>
                         <li><Link href="/team">Team</Link></li>
-                        <li><Link href="/team-details/1">Team Details</Link></li>
+                        {/* <li><Link href="/team-details/1">Team Details</Link></li> */}
                         <li><Link href="/pricing">Pricing</Link></li>
-                        <li><Link href="/faq">FAQ</Link></li>
-                        <li><Link href="/contact-us">Contact Us</Link></li>
-                        <li><Link href="/not-found">Error Page</Link></li>
+                        {/* <li><Link href="/faq">FAQ</Link></li> */}
+                        {/* <li><Link href="/contact-us">Contact Us</Link></li> */}
+                        {/* <li><Link href="/not-found">Error Page</Link></li> */}
                     </ul>
                 </li>
                 <li className="dropdown">
                     <Link href="project" className="dropdown-toggle" data-toggle="dropdown" onClick={toggleSubMenu}>Projects</Link>
                     <ul className="dropdown-menu">
                         <li><Link href="/project">Project style one</Link></li>
-                        <li><Link href="/project-details/1">Project Details</Link></li>
+                        {/* <li><Link href="/project-details/1">Project Details</Link></li> */}
                     </ul>
                 </li>
-                <li className="dropdown">
-                    <Link href="#" className="dropdown-toggle" data-toggle="dropdown" onClick={toggleSubMenu}>Services</Link>
-                    <ul className="dropdown-menu">
+                <li>
+                    <Link href="/services-2" className="dropdown-toggle" data-toggle="dropdown" onClick={toggleSubMenu}>Services</Link>
+                    {/* <ul className="dropdown-menu">
                         <li><Link href="/services">Services Version One</Link></li>
                         <li><Link href="/services-2">Services Version Two</Link></li>
                         <li><Link href="/services-details/1">Services Details</Link></li>
-                    </ul>
-                </li>
-                <li className="dropdown">
-                    <Link href="#" className="dropdown-toggle" data-toggle="dropdown" onClick={toggleSubMenu}>Blog</Link>
-                    <ul className="dropdown-menu">
-                        <li><Link href="/blog-standard">Blog Standard</Link></li>
-                        <li><Link href="/blog-with-sidebar">Blog With Sidebar</Link></li>
-                        <li><Link href="/blog-2-column">Blog Grid Two Colum</Link></li>
-                        <li><Link href="/blog-3-column">Blog Grid Three Colum</Link></li>
-                        <li><Link href="/blog-single/1">Blog Single</Link></li>
-                        <li><Link href="/blog-single-with-sidebar/1">Blog Single With Sidebar</Link></li>
-                    </ul>
+                    </ul> */}
                 </li>
                 <li><Link href="/contact-us">contact</Link></li>
             </ul>
