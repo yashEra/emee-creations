ac758e14f0aa8ad2cc27401e2cbc0abd4028647f
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2025-09-08 00:19:38 +0530

    emee teme name change

--- src/app/project-details/[id]/page.jsx
+++ src/app/project-details/[id]/page.jsx
@@ -1,24 +1,23 @@
-import LayoutStyle7 from '@/components/Layouts/LayoutStyle7';
-import ProjectDetailsContent from '@/components/project/ProjectDetailsContent';
-import React from 'react';
-import Project2Data from '@/assets/jsonData/project/Project2Data.json'
+import LayoutStyle7 from "@/components/Layouts/LayoutStyle7";
+import ProjectDetailsContent from "@/components/project/ProjectDetailsContent";
+import React from "react";
+import Project2Data from "@/assets/jsonData/project/Project2Data.json";
 
 export const metadata = {
-    title: "Consua - Consulting Business - Project Details"
-}
+  title: "emee - Consulting Business - Project Details",
+};
 
 const ProjectDetailsPage = ({ params }) => {
+  const { id } = params;
+  const data = Project2Data.find((project) => project.id === parseInt(id));
 
-    const { id } = params
-    const data = Project2Data.find(project => project.id === parseInt(id))
-
-    return (
-        <>
-            <LayoutStyle7 breadCrumb="Project-details" title="Project Details">
-                <ProjectDetailsContent projectInfo={data} />
-            </LayoutStyle7>
-        </>
-    );
+  return (
+    <>
+      <LayoutStyle7 breadCrumb="Project-details" title="Project Details">
+        <ProjectDetailsContent projectInfo={data} />
+      </LayoutStyle7>
+    </>
+  );
 };
 
 export default ProjectDetailsPage;
